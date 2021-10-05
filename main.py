6a8086aa7e78f7e953be4a4b9fd9f72f9121039e
Bast08 <65960788+Bast08@users.noreply.github.com>
2021-10-05 16:37:26 +0200

    Update main.py
    Adding new app route.

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 from flask import Flask
 
 app = Flask(__name__)
-@app.route('/')
 
+@app.route('/')
 def hello_world():
     return 'hello world!'
 
+@app.route('/scores')
+def scores():
+    return 'this is where i will interact with scores data'
+
+@app.route('/scores/<score_value>')
+def score_value(score_value):
+    return 'this is where i will interact with the value : ' + score_value
+
 if __name__ == '__main__':
     app.run()
-
