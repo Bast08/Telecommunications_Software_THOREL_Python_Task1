f9a6035ad416ba3b604dc3c2343ee4516325912b
Bast08 <65960788+Bast08@users.noreply.github.com>
2021-10-06 19:18:42 +0200

    Added GET method + adaptative response for scores

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask
 import json
+
 app = Flask(__name__)
 
 data = """{
@@ -8,28 +9,32 @@ data = """{
 "releaseYear": 2021,
 "courseActive": true,
 "droppedStudents": null,
-"date": 20210218,
+"date": 08-10-2021,
 "someData": [[11,2], [22, 4], [33, 1], [44,5]],
 "scores": {"a":77, "b":46, "c":91}
 }"""
 
 json_data = json.loads(data)
 
-@app.route('/')
+@app.route('/', methods = ['GET'])
 def hello_world():
     return 'hello world!'
 
-@app.route('/data')
+@app.route('/data', methods = ['GET'])
 def send_data():
     return json_data
 
-@app.route('/scores')
+@app.route('/scores', methods = ['GET'])
 def scores():
     return json_data["scores"]
 
-@app.route('/scores/<score_value>')
+@app.route('/scores/<score_value>', methods = ['GET'])
 def score_value(score_value):
-    return 'this is where i will interact with the value : ' + score_value
+    if score_value in ['a', 'b', 'c']:
+        return str(json_data["scores"][score_value])
+    else:
+        return "No data " + score_value + " !  Try with a, b or c !"
 
 if __name__ == '__main__':
     app.run()
+
