584e0c509efeafd1c55349cec331e06f2af6f461
Bast08 <65960788+Bast08@users.noreply.github.com>
2021-10-06 18:41:21 +0200

    Added the json response

--- main.py
+++ main.py
@@ -1,14 +1,31 @@
 from flask import Flask
-
+import json
 app = Flask(__name__)
+
+data = """{
+"course": 411,
+"courseName": "Software in Telecommunications",
+"releaseYear": 2021,
+"courseActive": true,
+"droppedStudents": null,
+"date": 20210218,
+"someData": [[11,2], [22, 4], [33, 1], [44,5]],
+"scores": {"a":77, "b":46, "c":91}
+}"""
+
+json_data = json.loads(data)
 
 @app.route('/')
 def hello_world():
     return 'hello world!'
 
+@app.route('/data')
+def send_data():
+    return json_data
+
 @app.route('/scores')
 def scores():
-    return 'this is where i will interact with scores data'
+    return json_data["scores"]
 
 @app.route('/scores/<score_value>')
 def score_value(score_value):
